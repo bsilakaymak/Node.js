0ba302a8d84243545ab3cfb9cbdf54d6d2433679
Sila <basaksilakaymak@gmail.com>
2019-11-21 18:57:57 +0100

    week-1-homework-Sila-Kaymak

--- week1/homework/nodejs-exercises/exercise-1/exercise-1.js
+++ week1/homework/nodejs-exercises/exercise-1/exercise-1.js
@@ -1,5 +1,5 @@
 const padLeft = require("./andrejs-awesome-function.js");
 const numbers = ["12", "846", "2", "1236"];
 numbers.forEach(function(number) {
-  console.log(padLeft(number, 8, ""));
+  console.log(padLeft(number, 4, ""));
 });
